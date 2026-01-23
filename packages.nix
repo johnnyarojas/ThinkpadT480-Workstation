69b14c8c25970e9a92bdf74a5291718e4006bac9
johnnyarojas <johnny.rojas.me@gmail.com>
2026-01-23 14:25:13 -0500

    Synced Packages across Workstations

--- packages.nix
+++ packages.nix
@@ -2,33 +2,73 @@
 
 {
   environment.systemPackages = with pkgs; [
+    ############################################
+    # Devloper Tools
+    ############################################
     vim
     neovim
-    firefox
-    wl-clipboard
-    youtube-music
-    zoom-us
-    lyx
-    texlive.combined.scheme-full
-    libreoffice-fresh
-    rnote
-    xdg-user-dirs
     emacs-gtk
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.coverflow-alt-tab
-    gnomeExtensions.grand-theft-focus
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.just-perfection
-    gnomeExtensions.vitals
+    git
+    ganttproject-bin
+ 
+    ############################################
+    # Office Tools
+    ############################################
+    libreoffice-fresh
+    lyx
+    rnote
+    texlive.combined.scheme-full
+
+    ############################################
+    # Music
+    ############################################
+    youtube-music
+    musescore
+    muse-sounds-manager
+
+    ############################################
+    # Graphics
+    ############################################
+    gimp
+    kdePackages.kdenlive
+    blender
+
+    ############################################
+    # Internet
+    ############################################
+    firefox
+
+    ############################################
+    # System
+    ############################################
+    wl-clipboard
+    xdg-user-dirs
     ntfs3g
     exfatprogs
     gparted
     rclone
     hunspell
     hunspellDicts.en_US
-    git
-    musescore
-    muse-sounds-manager
-    kdePackages.kdenlive
+    
+    ############################################
+    # Social
+    ############################################
+    zoom-us
+    signal-desktop
+    
+    ############################################
+    # GNOME Extensions
+    ############################################
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.coverflow-alt-tab
+    gnomeExtensions.grand-theft-focus
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.just-perfection
+    gnomeExtensions.vitals
+
+    ############################################
+    #  Games
+    ############################################
+    prismlauncher
   ];
  }
