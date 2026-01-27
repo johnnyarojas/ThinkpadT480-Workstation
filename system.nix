aea096f42bb27bd39e38d681588f0666fcdc4ef3
johnnyarojas <johnny.rojas.me@gmail.com>
2026-01-26 20:13:19 -0500

    Experimental Plymouth Theme

--- system.nix
+++ system.nix
@@ -11,6 +11,33 @@
   };
   boot.loader.efi.canTouchEfiVariables = true; # Allows NixOS to manage EFI variables
   boot.kernelPackages = pkgs.linuxPackages_latest; # Latest kernel
+  boot = {
+
+    plymouth = {
+      enable = true;
+      theme = "rings";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "rings" ];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+
+  };
 
   ############################################
   #  Hostname & Networking
