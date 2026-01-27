ce116ab422c40af46c3f4b6017caf5fabfbacf65
johnnyarojas <johnny.rojas.me@gmail.com>
2026-01-27 09:41:21 -0500

    Synced Commit for Home.nix of SurfacePro7

--- home.nix
+++ home.nix
@@ -3,6 +3,20 @@
 {
   home.stateVersion = "25.11";
 
+  programs.neovim = {
+    enable = true;
+
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+
+    extraLuaConfig = ''
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.opt.colorcolumn = "80"
+    '';
+  };
+
   home.packages = with pkgs; [
     (
       python311.withPackages (pythonPackages: [
